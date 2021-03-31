f06c7a54ff7d3a0e7062a01ebc5e0a78c75c3fa7
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2021-03-31 10:56:10 +0300

    structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
- 
+import PostForm from './components/PostForm' 
+import Posts from './components/Posts'  
+import FetchedPosts from './components/FetchedPosts' 
 
 function App() {
   return (
-    <div className="container">
-      <h1>React</h1>
+    <div className="container pt-3">
+      <div className="row">
+        <div className="col">
+          <PostForm />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <h3>Sync Posts</h3>
+          <Posts posts={[1, 2, 3]} />
+        </div>
+        <div className="col">
+          <h3>Async Posts</h3>
+          <FetchedPosts posts={[]} />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/FetchedPosts.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default () => {
+    return (
+        <div>
+          <h2>Fetched Posts</h2>
+        </div>
+    )
+}

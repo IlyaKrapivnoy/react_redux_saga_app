c307a7914bb743dc4a3b579b9093a0f96789a74c
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2021-03-31 13:24:15 +0300

    react-redux provider

--- src/index.js
+++ src/index.js
@@ -4,11 +4,17 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 import {rootReducer} from './redux/rootReducer';
 
 const store = createStore(rootReducer)
 
-render(<App />, document.getElementById('root'));
+const app = (
+    <Provider store={store}>
+        <App />
+    </Provider>
+)
+render(app, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

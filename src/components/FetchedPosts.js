fe1847b9656ed046ec48fb2f344fe69a501be781
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2021-04-01 12:07:43 +0300

    hide and show loader actions

--- src/components/FetchedPosts.js
+++ src/components/FetchedPosts.js
@@ -2,10 +2,18 @@ import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchPosts } from '../redux/actions'
 import Post from './Post'
+import {Loader} from './Loader'
 
 export default () => {
     const dispatch = useDispatch()
     const posts = useSelector(state => state.posts.fetchedPosts)
+    const loading = useSelector(state => state.app.loading)
+
+    if (loading) {
+      return (
+        <Loader />
+      )
+    }
      
     if (!posts.length) {
         return <button
